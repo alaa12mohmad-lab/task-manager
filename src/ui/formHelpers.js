e64878ab91105bae117f0918addf689e2eef8a20
alaa12mohmad-lab <alaa12mohmad@gmail.com>
2026-07-19 21:22:57 +0300

    Delete src directory

--- src/ui/formHelpers.js
+++
@@ -1,6 +0,0 @@
-export function setBtnLoading(id, loading, label = '') {
-  const btn = document.getElementById(id);
-  btn.disabled = loading;
-  if (loading) btn.innerHTML = '<div class="ld-spinner" style="width:16px;height:16px;border-width:2px;margin:0 auto"></div>';
-  else btn.innerHTML = label;
-}
